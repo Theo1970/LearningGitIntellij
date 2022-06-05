a6ebc31cac622d072f60c6d7f768517908ec093a
beneb70 <Theo70>
2022-06-05 21:52:34 +0200

    Initial Commit

---
+++ src/main/java/DevClass.java
@@ -0,0 +1,6 @@
+public class DevClass {
+
+    public static void main(String[] args) {
+        System.err.println ("DevClass");
+    }
+}
---
+++ src/test/java/DevTestClass.java
@@ -0,0 +1,6 @@
+public class DevTestClass {
+
+    public static void main(String[] args) {
+        System.err.println ("DevTestClass");
+    }
+}
