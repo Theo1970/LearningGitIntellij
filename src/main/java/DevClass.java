085113d986a9551139d631c971acc23a8f999530
Theo1970 <94944572+Theo1970@users.noreply.github.com>
2022-06-05 23:39:24 +0200

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,7 +1,7 @@
 public class DevClass {
 
     public static void main(String[] args) {
-        System.err.println ("DevClass+Stream2Learn");
+        System.err.println ("DevClass+Stream2Learn: more Commect Added");
         //Checking from Stream2 Learnfff
     }
 }
