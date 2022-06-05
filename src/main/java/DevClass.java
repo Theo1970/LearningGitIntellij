2a23d97f3f23dc7211289fc20359e12406235bfe
Theo1970 <94944572+Theo1970@users.noreply.github.com>
2022-06-05 23:16:33 +0200

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,6 +2,6 @@ public class DevClass {
 
     public static void main(String[] args) {
         System.err.println ("DevClass+Stream2Learn");
-        //Checking from Stream2 Learn
+        //Checking from Stream2 Learnfff
     }
 }
